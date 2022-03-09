16ec08159b01b39d76192a333140bcd95a1377a9
AmbrishAngular <ambrishramachandran07@gmail.com>
2022-03-09 17:27:20 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ApprouterModule } from './approuter/approuter.module';
+
+
+
+@NgModule({
+  imports: [BrowserModule, FormsModule,ApprouterModule],
+  
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
